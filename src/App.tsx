b2bbf4faf5ce9643dee458b686c0d8bda8af3762
Xavier Marques <xaviermarques4f@gmail.com>
2022-07-05 11:34:55 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Route, Router, Text, ButtonGroup, Button, useText, Image } from '@urban-bot/core';
+import fs from 'fs';
+import logo from './assets/logo.png';
+
+const file = fs.readFileSync(logo);
+
+function Echo() {
+    const [text, setText] = useState('Say something');
+
+    useText(({ text }) => {
+        setText(text);
+    });
+
+    return (
+        <Text>
+            <i>{text}</i>
+        </Text>
+    );
+}
+
+function Logo() {
+    const [title, setTitle] = useState('Urban Bot');
+
+    const addRobot = () => {
+        setTitle(title + '🤖');
+    };
+
+    return (
+        <Image
+            title={title}
+            file={file}
+            buttons={
+                <ButtonGroup>
+                    <Button onClick={addRobot}>Add robot</Button>
+                </ButtonGroup>
+            }
+        />
+    );
+}
+
+export function App() {
+    return (
+        <>
+            <Text>Welcome to Urban Bot! Type /echo or /logo.</Text>
+            <Router>
+                <Route path="/echo">
+                    <Echo />
+                </Route>
+                <Route path="/logo">
+                    <Logo />
+                </Route>
+            </Router>
+        </>
+    );
+}
